6ece1d1adc38293337df4c523aa76d6106d96508
shivamussr <shivamsharmaf52@gmail.com>
2025-11-26 11:44:26 +0530

    Add program to separate digits of a three-digit number
    Implements a program to separate and display the digits of a three-digit number.

---
+++ 3_saparete_digit.c
@@ -0,0 +1,23 @@
+//WAP to separate the digits of a three digit number
+#include <stdio.h>
+
+int main(void){
+    int k;
+
+    printf("Enter three digit number: ");
+    scanf("%d",&k);
+
+    while(k<100 || k>999){
+        printf("Invalid input please enter three digit number: ");
+        scanf("%d",&k);
+    }
+    
+    int hundreds = k / 100;
+    int tens = (k / 10) % 10;
+    int units = k % 10;
+
+    printf("Hundreds: %d\n", hundreds);
+    printf("Tens: %d\n", tens);
+    printf("Units: %d\n", units);
+
+}
