481290f7f50c2866d8233c2d8ce4c2c063d41949
shivamussr <shivamsharmaf52@gmail.com>
2025-11-26 11:25:09 +0530

    Implement arithmetic operators demonstration in C
    Demonstrates arithmetic operators in C with user input.

---
+++ 11_Arithmetic_operators.c
@@ -0,0 +1,26 @@
+//WAP to demostrate the arithmetic operators in c
+
+#include <stdio.h>
+
+int main(void)
+{
+        int a, b;
+        printf("Enter first number A: ");
+        scanf("%d", &a);
+        printf("Enter secound number B: ");
+        scanf("%d", &b);
+        
+        /* Arithmetic operators */
+
+        //sum
+        printf("a+b=%d \n", a+b);
+        //substraction
+        printf("a-b=%d \n", a-b);
+        //multiplication
+        printf("a*b=%d \n", a*b);
+        //modulas
+        printf("a%b=%d \n", a%b);
+        //division
+        printf("a/b=%d \n", a/b);
+
+} 
